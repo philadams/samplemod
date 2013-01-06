84ac0219c9239f1c6155b1b7a04e55717b534cd8
Phil Adams <phil@philadams.net>
2013-01-06 13:06:26 -0500

    personalize setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -14,9 +14,9 @@ setup(
     version='0.0.1',
     description='Sample package for Python-Guide.org',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Phil Adams',
+    author_email='phil@philadams.net',
+    url='https://github.com/philadams/samplemod',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
